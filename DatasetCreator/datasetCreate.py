e0fb6e3002865ee9153c154671002c31d27bcdb8
Miguel Angel <macominom@gmail.com>
2019-04-06 22:20:24 +0200

    Añadir generacion TFRecord

--- DatasetCreator/datasetCreate.py
+++ DatasetCreator/datasetCreate.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import os
 import pandas as pd
+from generate_tfrecord import createTFRecord
 
 class DatasetCreate:
 
@@ -13,7 +14,7 @@ class DatasetCreate:
     def saveCsv(self):
         column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
         df = pd.DataFrame(self.images_list, columns=column_name)
-        df.to_csv('annotations.csv', index=None)
+        df.to_csv(os.path.join(self.outputPath, 'annotations.csv'), index=None)
 
     def addImageToList(self, filename, width, height, clase, xmin, ymin, xmax, ymax):
         value = (filename,
@@ -28,8 +29,8 @@ class DatasetCreate:
         self.images_list.append(value)
 
     def initialize(self, imageFile, outputFile):
-        width = 500
-        height = 300
+        width = random.randint(300,1000)
+        height = random.randint(300,1000)
 
         imagen = cv2.imread(os.path.join(self.inputImagePath, imageFile))
 
@@ -37,7 +38,7 @@ class DatasetCreate:
         blank_image = np.zeros((height,width), np.uint8)
         blank_image[:,:] = 255
 
-        print(random.randint(1,101))
+        #print(random.randint(1,101))
 
         y_offset = random.randint(0,blank_image.shape[0] - imagen.shape[0])
         x_offset = random.randint(0,blank_image.shape[1] - imagen.shape[1])
@@ -50,7 +51,7 @@ class DatasetCreate:
         #print(type(imagen))
         #cv2.imshow("prueba", blank_image)
         #cv2.waitKey(0)
-        self.addImageToList(outputFile, width, height, imageFile, x_offset, y_offset, x_offset+imagen.shape[1], y_offset+imagen.shape[0])
+        self.addImageToList(outputFile, width, height, os.path.splitext(os.path.basename(imageFile))[0], x_offset, y_offset, x_offset+imagen.shape[1], y_offset+imagen.shape[0])
 
         cv2.imwrite( os.path.join(self.outputPath, outputFile) ,blank_image)
         return
@@ -65,12 +66,14 @@ if __name__ == '__main__':
 
         fileList = os.listdir(dc.inputImagePath)
 
-
+        # Creating dataset training
         for index in range(1,10):
             for file in fileList:
                 dc.initialize(file, str(index) + file)
         
         dc.saveCsv()
+
+        createTFRecord(os.path.join(dc.outputPath, 'train.record'), dc.outputPath, os.path.join(dc.outputPath, 'annotations.csv') )
         
     except ValueError:
         print(ValueError)
