25d5dc90a09433520a9c75c016204e59f62c9805
Miguel Angel <macominom@gmail.com>
2019-05-12 12:02:39 +0200

    Add process image and configuration

--- DetectionComponentsAPI/DetectAPI.py
+++ DetectionComponentsAPI/DetectAPI.py
@@ -6,6 +6,8 @@ import numpy as np
 import os
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
+import base64
+
 
 # Path to frozen detection graph .pb file, which contains the model that is used
 # for object detection.
@@ -14,14 +16,10 @@ PATH_TO_CKPT = '../../notebooktest/output/frozen_inference_graph.pb'
 # Path to label map file
 PATH_TO_LABELS = os.path.join('../DatasetCreator/out','label_map.pbtxt')
 
-# Path to image
-PATH_TO_IMAGE = '../../diagram127.jpg'
-
-
 # Number of classes the object detector can identify
 NUM_CLASSES = 13
 
-def init():
+def processImage(image):
 
     label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
     categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
@@ -37,16 +35,61 @@ def init():
 
     sess = tf.Session(graph=detection_graph)
 
+    # Input tensor is the image
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    # Output tensors are the detection boxes, scores, and classes
+    # Each box represents a part of the image where a particular object was detected
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    # Each score represents level of confidence for each of the objects.
+    # The score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+    # Number of objects detected
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    #image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
+    image_expanded = np.expand_dims(image, axis=0)
+
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: image_expanded})
+
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8,
+        min_score_thresh=0.50)
+
+    return cv2.imencode('.jpg', image)[1].tostring()
+
+
 def diagramDetection(environ, start_response):
 
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+    except (ValueError):
+        request_body_size = 0
 
+    request_body = environ['wsgi.input'].read(request_body_size)
 
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'Hello, world!']
+    nparr = np.fromstring(base64.b64decode(request_body), np.uint8)
+    img = cv2.imdecode(nparr, cv2.COLOR_BGR2GRAY)
+
+    
+    image_processed = processImage(img)
+
+    start_response('200 OK', [('Content-Type', 'image/jpeg;base64')])
+    return [image_processed]
 
 if __name__ == '__main__':
     try:
-        init()
         httpd = make_server('', 5050, diagramDetection)
         print('Serving on port 5050...')
         httpd.serve_forever()
