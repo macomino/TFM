9007aa59a8e811e6e220a0f44ba10c8a51c1a5f0
Miguel Angel <macominom@gmail.com>
2019-05-12 13:03:24 +0200

    Frontend initial

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatMenuModule, MatCardModule } from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { MatFileUploadModule } from 'angular-material-fileupload';
+ 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatFileUploadModule
+  ],
+  providers: [],
+  bootstrap: [MainComponent]
+})
+export class AppModule { }
